11063582fb6db6ea30f1195bd96271e571ea281c
orsusbass <orsusbass@gmail.com>
2019-07-09 15:05:33 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,20 +20,21 @@ defects = [["Air Compressor", true, false], ["Air Lines", true, false], ["Batter
            ["Tire Chains", true, false], ["Tires", true, true], ["Transmission", true, false], ["Trip Recorder", true, false], 
            ["Wheels/Rims", true, true], ["Windshield Wipers", true, false], ["Other", true, true]
           ]
-
-defects.each_with_index do |d, i|
-  Defect.create(name: defects[i][0], is_truck_defect: defects[i][1], is_trailer_defect: defects[i][2], is_active: true)
-end
-
-5.times do 
-  c = Company.create(name: FFaker::Company.name, provision_key: FFaker::Skill.tech_skill, user_cap: 5, street: FFaker::AddressUS.street_address, city: FFaker::AddressUS.city, state: FFaker::AddressUS.state_abbr, zipcode: FFaker::AddressUS.zip_code)
-  5.times do 
-    Asset.create(asset_number: rand(100..999), last_service_date: Time.now - rand(1..5).days, last_odo_reading: rand(1000..99999), last_odo_read_date: Time.now - rand(1..5).days, is_active: rand(0..1), company_id: c.id, type: 'Truck')
+  defects.each_with_index do |d, i|
+    Defect.create(name: defects[i][0], is_truck_defect: defects[i][1], is_trailer_defect: defects[i][2], is_active: true)
   end
+
+if !Rails.env.production?
+  5.times do 
+    c = Company.create(name: FFaker::Company.name, provision_key: FFaker::Skill.tech_skill, user_cap: 5, street: FFaker::AddressUS.street_address, city: FFaker::AddressUS.city, state: FFaker::AddressUS.state_abbr, zipcode: FFaker::AddressUS.zip_code)
     5.times do 
-    u = User.create(employee_number: rand(1000..1900), first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, contact_number: FFaker::PhoneNumber.short_phone_number, email: FFaker::Internet.email, account_type: rand(1..2), company_id: c.id)
-    5.times do
-      Report.create(is_complete: true, street: FFaker::AddressUS.street_address, city: FFaker::AddressUS.city, state: FFaker::AddressUS.state_abbr, zipcode: FFaker::AddressUS.zip_code, completion_date: Time.now - rand(1..15).days, user_id: u.id)
+      Asset.create(asset_number: rand(100..999), last_service_date: Time.now - rand(1..5).days, last_odo_reading: rand(1000..99999), last_odo_read_date: Time.now - rand(1..5).days, is_active: rand(0..1), company_id: c.id, type: 'Truck')
+    end
+      5.times do 
+      u = User.create(employee_number: rand(1000..1900), first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, contact_number: FFaker::PhoneNumber.short_phone_number, email: FFaker::Internet.email, account_type: rand(1..2), company_id: c.id)
+      5.times do
+        Report.create(is_complete: true, street: FFaker::AddressUS.street_address, city: FFaker::AddressUS.city, state: FFaker::AddressUS.state_abbr, zipcode: FFaker::AddressUS.zip_code, completion_date: Time.now - rand(1..15).days, user_id: u.id)
+      end
     end
   end
 end

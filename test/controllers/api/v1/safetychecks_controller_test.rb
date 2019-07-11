7cfb6bfda52f4f8709c1b9203d6830eb36169fff
orsusbass <orsusbass@gmail.com>
2019-07-11 11:38:38 -0400

    removing old test suite

--- test/controllers/api/v1/safetychecks_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Api::V1::SafetychecksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_safetychecks_index_url
-    assert_response :success
-  end
-
-end

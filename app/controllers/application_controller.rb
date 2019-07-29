9048266e3a2cf2c51bc0ff375ec0683ceb3bf8d2
orsusbass <orsusbass@gmail.com>
2019-07-28 23:54:18 -0400

    removing csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
   def after_sign_in_path_for(resource)
     if current_user.is_admin?
       admin_dashboard_path
